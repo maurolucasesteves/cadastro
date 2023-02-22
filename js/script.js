e3c1f47d82dfaa37f87538af1f2bfc0755ff770c
Mauro Lucas Esteves da Silva <50851060+maurolucasesteves@users.noreply.github.com>
2023-02-22 18:40:35 -0300

    Incio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function toggleMenu1() {
+    var menu1 = document.getElementById("menu1");
+    if (menu1.style.display === "block") {
+      menu1.style.display = "none";
+    } else {
+      menu1.style.display = "block";
+    }
+  }
+  
+  function toggleMenu2() {
+    var menu2 = document.getElementById("menu2");
+    if (menu2.style.display === "block") {
+      menu2.style.display = "none";
+    } else {
+      menu2.style.display = "block";
+    }
+  }
